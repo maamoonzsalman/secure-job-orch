3d4885c8cede9593b7988ee1dfad9dc7562bf299
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2026-01-06 18:21:53 -0800

    fix: typescript error

--- client/components/theme-switcher.tsx
+++ client/components/theme-switcher.tsx
@@ -55,7 +55,7 @@ const ThemeSwitcher = () => {
       <DropdownMenuContent className="w-content" align="start">
         <DropdownMenuRadioGroup
           value={theme}
-          onValueChange={(e) => setTheme(e)}
+          onValueChange={(value: string) => setTheme(value)}
         >
           <DropdownMenuRadioItem className="flex gap-2" value="light">
             <Sun size={ICON_SIZE} className="text-muted-foreground" />{" "}
